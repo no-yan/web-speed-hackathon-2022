f96397364128deb4dd167269a258fc37fff5ce7c
no-yan <okamoto-wataru@dmm.com>
2022-11-05 17:46:18 +0900

    test

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -6,4 +6,4 @@ import ReactDOM from "react-dom";
 import { App } from "./foundation/App";
 
 const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
+ReactDOM.render(<div>hello</div>, root);
