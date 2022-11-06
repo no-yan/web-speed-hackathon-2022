9318f9d34231554d9afdcb1c51cd1057ab033c54
no-yan <okamoto-wataru@dmm.com>
2022-11-06 23:55:44 +0900

    remove dataURL conversion

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -10,24 +10,24 @@ import React, { useEffect, useState } from "react";
  * @property {number} height
  */
 
-/** @type {(cv: Size, img: Size) => Size} */
-const calcImageSize = (cv, img) => {
-  const constrainedHeight = cv.width * (img.height / img.width);
+// /** @type {(cv: Size, img: Size) => Size} */
+// const calcImageSize = (cv, img) => {
+//   const constrainedHeight = cv.width * (img.height / img.width);
 
-  if (constrainedHeight >= cv.height) {
-    return {
-      height: constrainedHeight,
-      width: cv.width,
-    };
-  }
+//   if (constrainedHeight >= cv.height) {
+//     return {
+//       height: constrainedHeight,
+//       width: cv.width,
+//     };
+//   }
 
-  const constrainedWidth = cv.height * (img.width / img.height);
+//   const constrainedWidth = cv.height * (img.width / img.height);
 
-  return {
-    height: cv.height,
-    width: constrainedWidth,
-  };
-};
+//   return {
+//     height: cv.height,
+//     width: constrainedWidth,
+//   };
+// };
 
 /**
  * @typedef Props
@@ -38,32 +38,5 @@ const calcImageSize = (cv, img) => {
 
 /** @type {React.VFC<Props>} */
 export const TrimmedImage = ({ height, src, width }) => {
-  const [dataUrl, setDataUrl] = useState(null);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = src;
-    img.onload = () => {
-      const canvas = document.createElement("canvas");
-      canvas.width = width;
-      canvas.height = height;
-
-      const size = calcImageSize(
-        { height: canvas.height, width: canvas.width },
-        { height: img.height, width: img.width },
-      );
-
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(
-        img,
-        -(size.width - canvas.width) / 2,
-        -(size.height - canvas.height) / 2,
-        size.width,
-        size.height,
-      );
-      setDataUrl(canvas.toDataURL());
-    };
-  }, [height, src, width]);
-
-  return <img src={dataUrl} />;
+  return <img height={height} src={src} width={width} />;
 };
